2d08e1e45d726ea7c5a23347751a0e07cd523e57
Xavier Clavel <xrclavel@gmail.com>
2025-01-29 09:04:15 +0100

    front - filter recipes by ingredients

--- frontend/src/pages/recipe/list.vue
+++ frontend/src/pages/recipe/list.vue
@@ -1,5 +1,8 @@
 <template>
-  <v-container>
+    <v-card
+    class="ma-5 py-2 px-4"
+    rounded="lg"
+    >
     <v-chip-group
       v-model="chosenSource"
       column
@@ -30,8 +33,24 @@
       ></v-chip>
     </v-chip-group>
 
-
-  </v-container>
+      <v-combobox
+        v-model="selectedIngredients"
+        class="flex-grow-1"
+        color="primary"
+        :items="autocompleteList"
+        item-color="primary"
+        item-title="name"
+        item-value="id"
+        @update:search="(query) => onIngredientAutocompleteChange(query)"
+        :key="index"
+        return-object
+        multiple
+        chips
+        closable-chips
+        clearable
+        @update:modelValue="updateUrl"
+      ></v-combobox>
+    </v-card>
   <recipes-list :query="recipeQueries"></recipes-list>
 </template>
 
@@ -42,16 +61,25 @@ import {useAuthStore} from "@/stores/auth";
 const dishTypes = ["Entree","Main Dish","Desert","Other"]
 const dishClasses = ["ENTREE", "MAIN_DISH", "DESERT", "OTHER"]
 const source = ["My recipes", "Likes", "Cookbooks", "Follows"]
+const autocompleteList = ref([])
 
 const chosenDishType = ref([0, 1, 2])
 const chosenSource = ref([0,1])
+const selectedIngredients = ref([])
 
 const authStore = useAuthStore()
 
 const recipeQueries = ref("")
 import router from "@/router";
+import {searchIngredients} from "@/scripts/ingredients";
+
+const onIngredientAutocompleteChange = async (query) => {
+  const response = await searchIngredients(query, 0, 20);
+  autocompleteList.value = response.data.map(item => ({ id: item.id, name: item.name }));
+}
 
 const updateUrl = () => {
+  console.log(selectedIngredients)
   const route = router.currentRoute
   router.push({
     path: route.path, // Keep the current path
@@ -62,7 +90,8 @@ const updateUrl = () => {
         likedBy: chosenSource.value.includes(1) ? authStore.id : undefined,
         cookbookUser: chosenSource.value.includes(2) ? authStore.id : undefined,
         follows: chosenSource.value.includes(3) ? authStore.id : undefined,
-        dishClasses: chosenDishType.value.length > 0 ? chosenDishType.value.map(it => dishClasses[it]) .join(",") : undefined
+        dishClasses: chosenDishType.value.length > 0 ? chosenDishType.value.map(it => dishClasses[it]).join(",") : undefined,
+        ingredient: selectedIngredients.value.length > 0 ? selectedIngredients.value.map(it => it.id).join(",") : undefined
       }).filter(([_, value]) => value !== undefined) // Remove undefined values
     ),
   })
