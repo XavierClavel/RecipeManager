4b8e78123ecd0a6bae7c76bd94f7aa216a691889
Xavier Clavel <xrclavel@gmail.com>
2025-04-11 21:18:40 +0200

    errors localization

--- backend/src/main/kotlin/com/xavierclavel/exceptions/Exceptions.kt
+++ backend/src/main/kotlin/com/xavierclavel/exceptions/Exceptions.kt
@@ -9,48 +9,47 @@ class BadRequestException(cause: BadRequestCause): Exception(cause.key)
 class NotFoundException(cause: NotFoundCause): Exception(cause.key)
 
 enum class UnauthorizedCause(val key: String) {
-    SESSION_NOT_FOUND("session.not.found"),
-    USER_NOT_VERIFIED("user.not.verified"),
-    INVALID_PASSWORD("invalid.password"),
-    INVALID_TOKEN("invalid.token"),
+    SESSION_NOT_FOUND("session_not_found"),
+    USER_NOT_VERIFIED("user_not_verified"),
+    INVALID_PASSWORD("invalid_password"),
+    INVALID_TOKEN("invalid_token"),
 }
 
 enum class ForbiddenCause(val key: String) {
-    NOT_ALLOWED_TO_EDIT_RECIPE("not.allowed.to.edit.recipe"),
-    NOT_ALLOWED_TO_REMOVE_RECIPE("not.allowed.to.remove.recipe"),
-    NOT_ALLOWED_TO_EDIT_USER("not.allowed.to.edit.user"),
-    NOT_ALLOWED_TO_SEE_RECIPE("not.allowed.to.see.recipe"),
-    NOT_ALLOWED_TO_SEE_COOKBOOK("not.allowed.to.see.cookbook"),
-    NOT_ALLOWED_TO_SEE_USER("not.allowed.to.see.user"),
-    NOT_MEMBER_OF_COOKBOOK("not.member.of.cookbook"),
-    ACCOUNT_NOT_PUBLIC("account.not.public"),
-    MUST_BE_COOKBOOK_ADMINISTRATOR("must.be.cookbook.administrator"),
+    NOT_ALLOWED_TO_EDIT_RECIPE("not_allowed_to_edit_recipe"),
+    NOT_ALLOWED_TO_REMOVE_RECIPE("not_allowed_to_remove_recipe"),
+    NOT_ALLOWED_TO_EDIT_USER("not_allowed_to_edit_user"),
+    NOT_ALLOWED_TO_SEE_RECIPE("not_allowed_to_see_recipe"),
+    NOT_ALLOWED_TO_SEE_COOKBOOK("not_allowed_to_see_cookbook"),
+    NOT_MEMBER_OF_COOKBOOK("not_member_of_cookbook"),
+    ACCOUNT_NOT_PUBLIC("account_not_public"),
+    MUST_BE_COOKBOOK_ADMINISTRATOR("must_be_cookbook_administrator"),
 }
 
 enum class NotFoundCause(val key: String) {
-    USER_NOT_FOUND("user.not.found"),
-    RECIPE_NOT_FOUND("recipe.not.found"),
-    COOKBOOK_NOT_FOUND("cookbook.not.found"),
-    INGREDIENT_NOT_FOUND("ingredient.not.found"),
-    MAIL_NOT_FOUND("mail.not.found"),
-    FOLLOW_NOT_FOUND("follow.not.found"),
+    USER_NOT_FOUND("user_not_found"),
+    RECIPE_NOT_FOUND("recipe_not_found"),
+    COOKBOOK_NOT_FOUND("cookbook_not_found"),
+    INGREDIENT_NOT_FOUND("ingredient_not_found"),
+    MAIL_NOT_FOUND("mail_not_found"),
+    FOLLOW_NOT_FOUND("follow_not_found"),
 }
 
 enum class BadRequestCause (val key: String) {
-    INVALID_REQUEST("invalid.request"),
-    INVALID_IMAGE("invalid.image"),
+    INVALID_REQUEST("invalid_request"),
+    INVALID_IMAGE("invalid_image"),
 
-    TOKEN_MISSING("token.missing"),
-    MAIL_MISSING("mail.missing"),
+    TOKEN_MISSING("token_missing"),
+    MAIL_MISSING("mail_missing"),
 
-    USER_ALREADY_FOLLOWED("user.already.followed"),
-    USER_NOT_FOLLOWED("user.not.followed"),
-    NO_FOLLOW_REQUEST("no.follow.request"),
+    USER_ALREADY_FOLLOWED("user_already_followed"),
+    USER_NOT_FOLLOWED("user_not_followed"),
+    NO_FOLLOW_REQUEST("no_follow_request"),
 
-    NOT_APPLICABLE_ON_SELF("not.applicable.on.self"),
-    MAIL_ALREADY_USED("mail.already.used"),
-    USERNAME_ALREADY_USED("username.already.used"),
-    RECIPE_ALREADY_IN_COOKBOOK("recipe.already.in.cookbood"),
-    RECIPE_NOT_IN_COOKBOOK("recipe.not.in.cookbook"),
+    NOT_APPLICABLE_ON_SELF("not_applicable_on_self"),
+    MAIL_ALREADY_USED("mail_already_used"),
+    USERNAME_ALREADY_USED("username_already_used"),
+    RECIPE_ALREADY_IN_COOKBOOK("recipe_already_in_cookbook"),
+    RECIPE_NOT_IN_COOKBOOK("recipe_not_in_cookbook"),
 
 }
