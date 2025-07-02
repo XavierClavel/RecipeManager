e25880618694a9a571b88acd3967136fada91836
Xavier Clavel <xrclavel@gmail.com>
2025-07-02 20:25:36 +0200

    fixed missing user image in search

--- frontend/src/components/User.vue
+++ frontend/src/components/User.vue
@@ -20,7 +20,7 @@ defineProps({
           height="150px"
           class="mt-n3"
           rounded="t-lg"
-          :src="getUserIconUrl(user.id)"
+          :src="getUserIconUrl(user.id, user.version)"
           cover
         ></v-img>
         <v-card-title class="text-black text-h6 text-sm-h6 text-truncate font-weight-regular">{{ user.username }}</v-card-title>
