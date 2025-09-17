5cb870e3d8a4bab3ba83fb145daf74950f141917
Xavier Clavel <xrclavel@gmail.com>
2025-09-17 08:55:57 +0200

    app wip

--- app/scripts/recipes.ts
+++ app/scripts/recipes.ts
@@ -1,12 +1,64 @@
 
 export {
     getRecipe,
+    RecipeInfo,
+}
+type UserOverview = {
+    id: number,
+    role: string,
+    version: number,
+    username: string,
 }
 
-const getRecipe = async(id: number) => {
-    const response = await fetch('https://cooknco.eu/api/v1/recipe/10?locale=FR')
+type CustomIngredientInfo = {
+    name: string,
+    amount: number,
+    unit: string,
+}
+
+type RecipeIngredientInfo = {
+    id: number,
+    name: string,
+    amount: number,
+    unit: string,
+    complement: string,
+    type: string,
+    allowAmount: boolean,
+    allowWeight: boolean,
+    allowVolume: boolean,
+}
+
+class RecipeInfo {
+    constructor(
+        public id: number =-1,
+        public version: number = -1,
+        public title: string = '',
+        public dishClass: string = '',
+        public owner: UserOverview,
+        public description: string,
+
+        public preparationTime: number,
+        public cookingTime: number,
+        public cookingTemperature: number,
+
+        public ingredients: RecipeIngredientInfo[],
+        public customIngredients: CustomIngredientInfo[],
+        public steps: string[],
+        public tips: string,
+
+        public creationDate: number,
+        public editionDate: number,
+
+        public likesCount: number,
+    ) {}
+}
+
+async function getRecipe(id: number) {
+    console.log("making rest call")
+    const response = await fetch(`https://cooknco.eu/api/v1/recipe/${id}?locale=FR`)
     const json = await response.json()
     console.log(json)
+    return json as RecipeInfo
 }
 
 const listRecipes = async() => {
